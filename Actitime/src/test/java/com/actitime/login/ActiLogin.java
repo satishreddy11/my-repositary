8f8d66b2492fe8ce2ad517277c89b2ba70d704b9
sanik <sanik@LAPTOP-2Q9A1CVU>
2020-08-11 16:35:27 +0530

    my commit

---
+++ Actitime/src/test/java/com/actitime/login/ActiLogin.java
@@ -0,0 +1,12 @@
+package com.actitime.login;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+@Test
+public class ActiLogin {
+	public void run() {
+		Reporter.log("Hi Hello", true);
+		Reporter.log("Hi Hello", true);
+		Reporter.log("Hi Hello", true);
+	}
+}
