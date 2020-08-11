e418a609277e290cd2a2de6b530f0a72ef56f71c
sanik <sanik@LAPTOP-2Q9A1CVU>
2020-08-11 16:48:13 +0530

    commit

--- Actitime/src/test/java/com/actitime/login/ActiLogin.java
+++ Actitime/src/test/java/com/actitime/login/ActiLogin.java
@@ -7,6 +7,9 @@ public class ActiLogin {
 	public void run() {
 		Reporter.log("Hi Hello", true);
 		Reporter.log("Hi Hello", true);
+		Reporter.log("Hi Hello", true);
+		Reporter.log("Hi Hello", true);
+	
 	
 	}
 }
